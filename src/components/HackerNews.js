58735ed8a0178f12032930db1764045d2721965c
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-05-01 18:16:03 -0400

    added Hacker News component to display search and search results

---
+++ src/components/HackerNews.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+import Results from './Results'
+
+function HackerNews() {
+    return (
+        <div>
+           <SearchForm/> 
+           <Results/>      
+        </div>
+    )
+}
+
+export default HackerNews
