577efda847b4a58cef6cb2a71c55cb943a0a3529
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-05-01 18:15:03 -0400

    added general search API function

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,8 +1,10 @@
+import NoResults from "../components/NoResults";
+
 const searchAPI = async (term) => {
     // dispatch state
     const search = await fetch(`https://hn.algolia.com/api/v1/search?query=${term}`);
     const {hits} = await search.json();
-    console.log(hits)
+    return !hits.length ? console.log("i got nothin") : console.log(hits)
 }
 
 export default searchAPI
